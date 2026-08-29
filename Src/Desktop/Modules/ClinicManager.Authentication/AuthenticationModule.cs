6efb6795e0fa6069725e008598598bed51255649
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-08-29 09:31:47 +0300

    Last Sync: 2026-08-29 09:31 (Mobile)

---
+++ Src/Desktop/Modules/ClinicManager.Authentication/AuthenticationModule.cs
@@ -0,0 +1,22 @@
+using ClinicManager.Core.Constants;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace ClinicManager.Authentication;
+
+public class AuthenticationModule : IModule
+{
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        // Example:
+        // containerRegistry.RegisterForNavigation<AuthenticationView, AuthenticationViewModel>();
+        // containerRegistry.Register<IAuthenticationService, AuthenticationService>();
+    }
+
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+        // Example:
+        // var regionManager = containerProvider.Resolve<IRegionManager>();
+        // regionManager.RequestNavigate(RegionNames.MainRegion, nameof(AuthenticationView));
+    }
+}
