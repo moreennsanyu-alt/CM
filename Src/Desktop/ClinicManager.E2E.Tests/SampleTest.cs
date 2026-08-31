b72794e6e4f4e7c0a8b658b69d7b48d932c0ee01
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-08-31 08:39:10 +0300

    Add initial NUnit test class with setup and teardown

---
+++ Src/Desktop/ClinicManager.E2E.Tests/SampleTest.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace YourProject.Tests
+{
+    [TestFixture]
+    public class EmptyTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Code here runs before each test (e.g., initializing objects)
+        }
+
+        [Test]
+        public void TestMethodName()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Code here runs after each test (e.g., disposing objects)
+        }
+    }
+  
+}
