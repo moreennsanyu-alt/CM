ccc68d2ea4dd85008943684c33a8bf241ac1d288
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-02 06:23:34 +0300

    Update and rename .none to GlobalUsings.cs

---
+++ Src/Desktop/ClinicManager.E2E.Tests/GlobalUsings.cs
@@ -0,0 +1,18 @@
+global using FlaUI.Core;
+global using FlaUI.Core.Capturing;
+global using FlaUI.Core.Input;
+global using FlaUI.Core.Tools;
+global using FlaUI.UIA3;
+global using FlaUI.Core.AutomationElements;
+global using FlaUI.Core.WindowsAPI;
+global using FlaUI.TestUtilities;
+global using NUnit.Framework;
+global using OperatingSystem = FlaUI.Core.Tools.OperatingSystem;
+global using ClinicManager.E2E.Tests.Core;
+global using ClinicManager.E2E.Tests.Core.Controls;
+global using ClinicManager.E2E.Tests.Core.SystemViews;
+
+
+
+
+
