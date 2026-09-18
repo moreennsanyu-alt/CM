c39f750e4302182002691c09a895e2b19751a655
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-18 08:38:57 +0300

    Update LoginDialogWindow.xaml.cs

--- Src/Desktop/ClinicManager.Win/Features/Authentication/Views/LoginDialogWindow.xaml.cs
+++ Src/Desktop/ClinicManager.Win/Features/Authentication/Views/LoginDialogWindow.xaml.cs
@@ -1,1 +1,23 @@
 
+namespace ClinicManager.Win.Features.Authentication.Views;
+
+public partial class LoginDialogWindow : Window, IDialogWindow
+{
+    public LoginDialogWindow()
+    {
+        InitializeComponent();
+    }
+
+    public IDialogResult Result { get; set; }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
