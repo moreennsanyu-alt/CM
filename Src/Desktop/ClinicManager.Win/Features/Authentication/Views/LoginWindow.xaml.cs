ccba6dc7fe7e0e7659e6f9af1b293149072f3bcc
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-18 17:39:11 +0300

    Rename LoginView.xaml.cs to LoginWindow.xaml.cs

---
+++ Src/Desktop/ClinicManager.Win/Features/Authentication/Views/LoginWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using System.Windows.Controls;
+using ClinicManager.Win.Features.Authentication.ViewModels;
+
+namespace ClinicManager.Win.Features.Authentication.Views;
+
+public partial class LoginWindow : ThemedWindow
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
+
