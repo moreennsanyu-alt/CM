4a35410622ab41950ceba995e2a28daec06a94f9
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-18 14:45:14 +0300

    Update LoginViewModel.cs

--- Src/Desktop/ClinicManager.Win/Features/Authentication/ViewModels/LoginViewModel.cs
+++ Src/Desktop/ClinicManager.Win/Features/Authentication/ViewModels/LoginViewModel.cs
@@ -5,7 +5,7 @@ using Prism.Services.Dialogs;
 
 namespace ClinicManager.Win.Features.Authentication.ViewModels;
 
-public sealed class LoginViewModel : BindableBase, IDialogAware
+public partial sealed class LoginViewModel : BindableBase, IDialogAware
 {
     private readonly IAuthenticationService _authenticationService;
 
